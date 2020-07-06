6e5db2392456b7f1afa0a41c488924cac44e09e6
Maxime COMINOTTO <cominotto.maxime@gmail.com>
2020-07-06 09:24:47 +0700

    First check-in: Northwind entities and DB access class. Used to generate NuGet Package for the Northwind MVC and Northwind WebService Projects.

---
+++ NorthwindMVC.DAL/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace NorthwindDAL.Entities
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+
+        public List<Product> Products { get; set; }
+    }
+}
